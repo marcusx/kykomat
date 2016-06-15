fc5fb8acfe7c4aafab362e8f54a46ce4bbd14b73
Marcus Exner <marcus.exner@wunderkraut.com>
2016-06-15 18:42:24 +0200

    Adding first version of angularjs app.

---
+++ src/js/kykomat.js
@@ -0,0 +1,27 @@
+var kykomat = angular.module('kykomat', []);
+
+kykomat.controller('mainController', function ($scope, $http) {
+        
+    $http.get('/src/data/materials.json').success(function (data) {
+        $scope.materials = data.materials;
+    });
+    $http.get('/src/data/material-categories.json').success(function (data) {
+        $scope.materialCategories = data.materialCategories;
+    });
+    $http.get('/src/data/students.json').success(function (data) {
+        $scope.students = data.students;
+    });    
+});
+
+kykomat.directive('material', function () {
+    return {
+        templateUrl: 'templates/material.html'
+    };
+});
+
+kykomat.directive('categoryBox', function () {
+    return {
+        transclude: true,
+        templateUrl: 'templates/category-box.html'
+    };
+});
